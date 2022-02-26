27b15aea621ae7af2154c89036e175c8a7ebd92e
mukulsinghal407 <mukulsinghal407@gmail.com>
2022-02-26 21:38:49 +0530

    changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const ejs = require("ejs");
 const moongoose = require("mongoose");
 const { default: mongoose } = require("mongoose");
 
-moongoose.connect("mongodb+srv://admin-mukul:<password>@cluster0.tj2jy.mongodb.net/hacktu3",{ useNewUrlParser: true, useUnifiedTopology: true });
+moongoose.connect("mongodb+srv://admin-mukul:Test123@cluster0.tj2jy.mongodb.net/hacktu3",{ useNewUrlParser: true, useUnifiedTopology: true });
 
 //Setting things up
 const app = express();
@@ -36,7 +36,7 @@ const teacher = new mongoose.Schema({
 
 const userStudent = mongoose.model("Student",Student);
 const userTeacher = mongoose.model("Teachers",teacher);
-const subject = mongoose.model("Subjects",subject);
+const Subject = mongoose.model("Subjects",subject);
 
 const Students=[
     {
@@ -46,16 +46,21 @@ const Students=[
         email:"ndvi@jdgv.com"
     },
     {
-        
+
     }
 ]
 app.get("/",function(req,res){
     res.render("dashboard",{info:["HELLO hell","hi ","idvuwjdvbinwlv"]});
 });
 
-app.post("/",(req,res)=>
-{
+app.get("/my/:subject",function(req,res){
+    console.log(req.params.subject);
+});
 
+app.post("/subject",(req,res)=>
+{
+    console.log(req.body.one);
+    console.log(req.body.two);
 });
 
 app.listen(process.env.PORT||3000,(req,res)=>
